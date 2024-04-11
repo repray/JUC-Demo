e015caf61bb3abd49402cc86edb3120093d4db77
Li.Yang <806446856@qq.com>
2024-04-11 17:38:12 +0800

    增加 MapTest类 添加 map类并发操作解决方案

---
+++ src/com/liang/juc/MapTest.java
@@ -0,0 +1,25 @@
+package com.liang.juc;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MapTest {
+    public static void main(String[] args) {
+        //默认等价于 new HashMap<>(16,0.75)
+        /**
+         * 并发解决方案:
+         * new ConcurrentHashMap<>();
+         * Collections.synchronizedMap(new ConcurrentHashMap<>())
+         */
+        Map<String, String> map = Collections.synchronizedMap(new ConcurrentHashMap<>());
+        for (int i = 0; i < 30; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 5));
+                System.out.println(map);
+            }, String.valueOf(i)).start();
+        }
+    }
+}
